b38b848d09bf1615a955a5013412d4ace20277e6
Limitpointinf0 <coneal0523@protonmail.com>
2024-04-08 18:51:15 +0000

    Updates

--- assets/use-auth-8a42a5e8.js
+++
@@ -1,1 +0,0 @@
-import{n as s,r,z as e}from"./index-db254d9a.js";const n=()=>{const t=s();return r.useEffect(()=>{e.get("jwt")||t.push("/login")},[t]),null};export{n as u};
---
+++ assets/use-auth-8e16ffd7.js
@@ -0,0 +1,1 @@
+import{w as s,r,G as e}from"./index-c2df3293.js";const a=()=>{const t=s();return r.useEffect(()=>{e.get("jwt")||t.push("/login")},[t]),null};export{a as u};
